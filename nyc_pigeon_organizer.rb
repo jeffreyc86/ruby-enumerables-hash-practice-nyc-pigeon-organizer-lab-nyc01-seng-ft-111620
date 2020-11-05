5bc2845de4398a78b8d121ae3eaa0434dd5051e4
Jeffrey Chiu <mr.jeffrey.chiu@gmail.com>
2020-11-05 19:30:33 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,6 +4,8 @@ def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, value), final_hash|
     value.each do |inner_key, names|
      names.each do |name|
+       if !final_hash[name]
+         final_hash[name] = {}
     binding.pry
     end
     end
