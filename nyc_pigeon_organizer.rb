dd70706110779db5645db558e43fef8d5a03f9f1
Jeffrey Chiu <mr.jeffrey.chiu@gmail.com>
2020-11-05 19:27:56 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,8 +2,8 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, value), final_hash|
-  binding.pry
     value.each do |inner_key, names|
+      binding.pry
     end
   end
 end
